f7b7c704a55c5dc871e0af2ed700552b5d91b886
Sergey Protasov <protasov.s.s@gmail.com>
2020-10-13 00:17:34 +0300

    Fight with composer versions

---
+++ packages/eloquent/src/LaravelServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Quiz\Eloquent;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        // load migrations code
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
